6b9f587afa1f7a31d75938ce1aa98010b9d64ca6
Sibin-Thomas <sibintho007@gmail.com>
2022-02-28 02:32:48 +0530

    updated attributes

--- backend/src/main/java/com/datamasking/controllers/DataConfigurationController.java
+++ backend/src/main/java/com/datamasking/controllers/DataConfigurationController.java
@@ -1,14 +1,17 @@
 package com.datamasking.controllers;
 
 import com.datamasking.helperClasses.DataConfigurationReqestBody;
+import com.datamasking.services.ConfigurationFileGeneratorService;
 import org.springframework.web.bind.annotation.*;
+
+import javax.xml.parsers.ParserConfigurationException;
 
 @RestController
 public class DataConfigurationController {
 
     @PostMapping("/configuration")
-    String sendConfiguration(@RequestBody DataConfigurationReqestBody dataConfigurationReqestBody)
-    {
+    String sendConfiguration(@RequestBody DataConfigurationReqestBody dataConfigurationReqestBody) throws ParserConfigurationException {
+        new ConfigurationFileGeneratorService().createConfiguration(dataConfigurationReqestBody);
         return dataConfigurationReqestBody.toString();
     }
 }
