1cca9da8ffc71b509c5a0e9ffa3db8649cf68383
Sibin-Thomas <sibintho007@gmail.com>
2022-02-28 01:45:01 +0530

    added application files

---
+++ backend/src/main/java/com/datamasking/DatamaskingApplication.java
@@ -0,0 +1,13 @@
+package com.datamasking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatamaskingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatamaskingApplication.class, args);
+	}
+
+}
