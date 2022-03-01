149063f3fd8f5746f6a5b893dee2e87b7dae12fd
Naidu2658 <kodanda.brahma@iiitb.ac.in>
2022-03-01 17:08:25 +0530

    changes to integrate frontend

--- backend/src/main/java/com/datamasking/controllers/DataConfigurationController.java
+++ backend/src/main/java/com/datamasking/controllers/DataConfigurationController.java
@@ -2,16 +2,22 @@ package com.datamasking.controllers;
 
 import com.datamasking.helperClasses.DataConfigurationReqestBody;
 import com.datamasking.services.ConfigurationFileGeneratorService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.xml.parsers.ParserConfigurationException;
-
+@RequestMapping
 @RestController
+@CrossOrigin
 public class DataConfigurationController {
 
-    @PostMapping("/configuration")
-    String sendConfiguration(@RequestBody DataConfigurationReqestBody dataConfigurationReqestBody) throws ParserConfigurationException {
+
+    @RequestMapping(value = "/configuration", method = RequestMethod.POST)
+    public ResponseEntity<?> sendConfiguration(@RequestBody DataConfigurationReqestBody dataConfigurationReqestBody) throws ParserConfigurationException {
+       System.out.println("hii");
+       System.out.println(dataConfigurationReqestBody.toString());
         new ConfigurationFileGeneratorService().createConfiguration(dataConfigurationReqestBody);
-        return dataConfigurationReqestBody.toString();
+        Object entity=null;
+        return ResponseEntity.ok("entity");
     }
 }
