74f0a90ca258172ed00c07e51795094606650bbd
Sibin-Thomas <sibintho007@gmail.com>
2022-03-15 14:42:43 +0530

    added helper classes

--- backend/src/main/java/com/datamasking/controllers/DataConfigurationController.java
+++ backend/src/main/java/com/datamasking/controllers/DataConfigurationController.java
@@ -1,10 +1,20 @@
 package com.datamasking.controllers;
 
 import com.datamasking.helperClasses.DataConfigurationReqestBody;
+import com.datamasking.helperClasses.XmlUploadResponseBody;
 import com.datamasking.services.ConfigurationFileGeneratorService;
+import org.apache.ws.commons.schema.XmlSchema;
+import org.apache.ws.commons.schema.XmlSchemaCollection;
+import org.apache.ws.commons.schema.XmlSchemaElement;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.xml.namespace.QName;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.stream.StreamSource;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.Map;
 
 @RestController
 public class DataConfigurationController {
@@ -13,5 +23,27 @@ public class DataConfigurationController {
     String sendConfiguration(@RequestBody DataConfigurationReqestBody dataConfigurationReqestBody) throws ParserConfigurationException {
         new ConfigurationFileGeneratorService().createConfiguration(dataConfigurationReqestBody);
         return dataConfigurationReqestBody.toString();
+    }
+
+    @PostMapping("/xmlUpload")
+    @CrossOrigin
+    XmlUploadResponseBody sendElements(@RequestParam("file") MultipartFile file)
+    {
+        XmlUploadResponseBody xmlUploadResponseBody = new XmlUploadResponseBody("");
+        if (!file.isEmpty())
+        {
+            try {
+                byte[] bytes = file.getBytes();
+                String xmlString = new String(bytes);
+                System.out.println(xmlString);
+                xmlUploadResponseBody.setElements(xmlString);
+                return xmlUploadResponseBody;
+            }
+            catch (IOException e)
+            {
+                System.out.println(e.getMessage());
+            }
+        }
+        return xmlUploadResponseBody;
     }
 }
---
+++ backend/src/main/java/com/datamasking/helperClasses/XmlUploadResponseBody.java
@@ -0,0 +1,17 @@
+package com.datamasking.helperClasses;
+
+public class XmlUploadResponseBody {
+    String elements;
+
+    public XmlUploadResponseBody(String elements) {
+        this.elements = elements;
+    }
+
+    public String getElements() {
+        return elements;
+    }
+
+    public void setElements(String elements) {
+        this.elements = elements;
+    }
+}
