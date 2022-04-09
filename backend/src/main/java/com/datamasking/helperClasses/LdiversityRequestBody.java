856f8d2d4b8679513d4947c15064b22deb407fe6
Naidu2658 <kodanda.brahma@iiitb.ac.in>
2022-04-09 23:19:20 +0530

    implemented l-diversity algo

--- backend/src/main/java/com/datamasking/helperClasses/LdiversityRequestBody.java
+++ backend/src/main/java/com/datamasking/helperClasses/LdiversityRequestBody.java
@@ -4,19 +4,40 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
 
-public class KAnonymityRequestBody {
+public class LdiversityRequestBody {
     MultipartFile xmlFile;
     Integer k;
-    ArrayList<String> xPaths;
 
-    public KAnonymityRequestBody()
-    {
-
+    public Integer getK() {
+        return k;
     }
-    public KAnonymityRequestBody(MultipartFile xmlFile, Integer k, ArrayList<String> xPaths) {
+
+    Integer l;
+    ArrayList<String> xPaths;
+    String sas; //sensitive attributes
+
+    public Integer getL() {
+        return l;
+    }
+
+    public void setL(Integer l) {
+        this.l = l;
+    }
+
+    public LdiversityRequestBody(MultipartFile xmlFile, Integer k, Integer l, ArrayList<String> xPaths, String sas) {
         this.xmlFile = xmlFile;
         this.k = k;
+        this.l = l;
         this.xPaths = xPaths;
+        this.sas = sas;
+    }
+
+    public void setSas(String sas) {
+        this.sas = sas;
+    }
+
+    public String getSas() {
+        return sas;
     }
 
     public MultipartFile getXmlFile() {
@@ -27,12 +48,10 @@ public class KAnonymityRequestBody {
         this.xmlFile = xmlFile;
     }
 
-    public Integer getK() {
-        return k;
-    }
 
-    public void setK(Integer k) {
-        this.k = k;
+
+    public void setK(Integer l) {
+        this.l = l;
     }
 
     public ArrayList<String> getxPaths() {
