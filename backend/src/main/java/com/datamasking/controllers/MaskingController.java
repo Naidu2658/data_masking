f2a1fca6e5d314dea00ad9a97ed307ac7ddf4280
sagarshirke2674 <sagarshirke2674@gmail.com>
2022-04-11 14:50:14 +0530

    Added T-closeness

--- backend/src/main/java/com/datamasking/controllers/MaskingController.java
+++ backend/src/main/java/com/datamasking/controllers/MaskingController.java
@@ -4,10 +4,8 @@ package com.datamasking.controllers;
 import com.datamasking.helperClasses.KAnonymityRequestBody;
 import com.datamasking.helperClasses.LdiversityRequestBody;
 import com.datamasking.helperClasses.MaskingRequestBody;
-import com.datamasking.services.ArrayListToXMLService;
-import com.datamasking.services.KAnonymityService;
-import com.datamasking.services.LdiversityService;
-import com.datamasking.services.MaskingService;
+import com.datamasking.helperClasses.TclosenessRequestBody;
+import com.datamasking.services.*;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -54,6 +52,34 @@ public class MaskingController {
 //        }
 //        System.out.println("anon:"+ xmlString);
         new ArrayListToXMLService().buildXMLFromArrayList(new LdiversityService().applyAlgorithm(ldrb));
+
+        return "";
+
+
+    }
+
+    @PostMapping("/applytcloseness")
+    @CrossOrigin(origins = {"*"})
+    String applyTcloseness(TclosenessRequestBody ldrb) throws ParserConfigurationException
+    {
+        System.out.println("hii");
+        KAnonymityRequestBody kanrb= new KAnonymityRequestBody();
+        kanrb.setK(ldrb.getK());
+        kanrb.setxPaths(ldrb.getxPaths());
+        kanrb.setXmlFile((ldrb.getXmlFile()));
+
+        new ArrayListToXMLService().buildXMLFromArrayList(new KAnonymityService().applyAlgorithm(kanrb));
+//        String xmlString = "";
+//        try {
+//            File file = new File("Anonymized.xml");
+//            xmlString = new String(Files.readAllBytes(file.toPath()));
+//        }
+//        catch (Exception e)
+//        {
+//            e.printStackTrace();
+//        }
+//        System.out.println("anon:"+ xmlString);
+        new ArrayListToXMLService().buildXMLFromArrayList(new TclosenessService().applyAlgorithm(ldrb));
 
         return "";
 
